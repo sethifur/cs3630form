cfb67288a4fcc827c61439e7d0fc7851e6dd4d96
sethifur <sethjohns@mail.weber.edu>
2017-03-22 11:54:53 -0600

    better Error handling

--- src/Form.js
+++ src/Form.js
@@ -25,8 +25,50 @@ class Form extends React.Component{
 
 
     onSubmit(){
-      console.log(this.state);
-      this.props.x(this.state);
+      const eVal = /\S+@\S+\.\S+/;
+      const pVal = /^(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$/;
+
+
+      if(this.state.fName !== ''){
+        document.getElementById("invaidFName").innerHTML = "";
+        if(this.state.lName !== ''){
+          document.getElementById("invaidLName").innerHTML = "";
+          if(this.state.email.match(eVal)){
+            document.getElementById("invaidEmail").innerHTML = "";
+            if(this.state.phone.match(pVal)){
+              document.getElementById("invaidFName").innerHTML = "";
+              document.getElementById("invaidLName").innerHTML = "";
+              document.getElementById("invaidEmail").innerHTML = "";
+              document.getElementById("invaidPhone").innerHTML = "";
+                this.props.x(this.state);
+            }
+            else{
+              console.log('false phone');
+              document.getElementById("invaidPhone").innerHTML = "\tEnter Phone Number (888-888-8888)";
+              document.getElementById("invaidPhone").style.color = "red";
+              document.getElementById("invaidPhone").style.fontSize = "10px";
+            }
+          }
+          else{
+            console.log('false email');
+            document.getElementById("invaidEmail").innerHTML = "\tEnter Email Address (example@website.com)";
+            document.getElementById("invaidEmail").style.color = "red";
+            document.getElementById("invaidEmail").style.fontSize = "10px";
+          }
+        }
+        else{
+          console.log('false lName');
+          document.getElementById("invaidLName").innerHTML = "\tEnter Last Name";
+          document.getElementById("invaidLName").style.color = "red";
+          document.getElementById("invaidLName").style.fontSize = "10px";
+        }
+      }
+      else{
+        console.log('false fName');
+        document.getElementById("invaidFName").innerHTML = "\tEnter First Name";
+        document.getElementById("invaidFName").style.color = "red";
+        document.getElementById("invaidFName").style.fontSize = "10px";
+      }
     }
 
 
@@ -38,22 +80,26 @@ class Form extends React.Component{
 				        <label for="fName">First Name: </label>
                 <input name="fName" type="text" onChange={this.onChange} value={this.state.fName}
                   oninvalid="setCustomValidity('First Name is required')"required/>
+                <span id="invaidFName"></span>
 				        <br />
 				        <br />
 				        <label for="lName">Last Name: </label>
 				        <input name="lName" type="text"  onChange={this.onChange} value={this.state.lName}
                   oninvalid="setCustomValidity('Last Name is required')" required/>
+                  <span id="invaidLName"></span>
                 <br />
 				        <br />
 				        <label for="email">Email: </label>
 				        <input name="email" type="email" onChange={this.onChange} value={this.state.email}
                   oninvalid="setCustomValidity('Enter Email Address (example@website.com)')" required/>
+                  <span id="invaidEmail"></span>
                 <br />
 				        <br />
 				        <label for="phone">Phone: </label>
 				        <input type="tel" name="phone" pattern="^(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$"
-                  oninvalid="setCustomValidity('Enter Phone Number (888-888-8888)')" onChange={this.onChange}
+                  oninvalid="alert('Enter Phone Number (888-888-8888)')" onChange={this.onChange}
                   value={this.state.phone} required/>
+                  <span id="invaidPhone"></span>
                 <br />
 				        <br />
 				        <input type="submit" value="Submit" onClick={this.onSubmit.bind(this)}/>
